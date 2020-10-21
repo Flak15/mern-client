1b7b3b6bcb501e2444ce47c44c759c0ed9a02757
Flak15 <flak160@gmail.com>
2020-10-21 19:45:43 +0300

    http hook

---
+++ src/hooks/http.hook.js
@@ -0,0 +1,33 @@
+import { useState, useCallback } from 'react';
+
+export const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const makeRequest = useCallback(
+    async (url, method = 'GET', body = null, headers = {}) => {
+      setIsLoading(true);
+      if (body) {
+        body = JSON.stringify(body);
+        headers['Content-Type'] = 'application/json';
+      }
+      try {
+        const res = await fetch(url, { method, body, headers });
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.message || 'something went wrong during the query');
+        }
+        setIsLoading(false);
+        return data;
+      } catch (e) {
+        setIsLoading(false);
+        setError(e.message);
+        throw e;
+      }
+    },
+    []
+  );
+  const cleanError = () => setError(null);
+  return { isLoading, error, makeRequest, cleanError };
+}
--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,9 +1,20 @@
 import React, { useState } from 'react';
+import { useHttp } from '../hooks/http.hook';
 
 export default () => {
-  const [form, useForm] = useState({ email: '', password: '' });
+  const { isLoading, error, makeRequest } = useHttp();
+  const [form, setForm] = useState({ email: '', password: '' });
 
+  const handleChange = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
   
+  const handleRegister = async () => {
+    try {
+      const data = await makeRequest('/api/register', 'POST', { ...form }); // must be api/auth/register
+      console.log(data)
+    } catch (e) {}
+  };
 
   return (
     <div>
@@ -15,26 +26,25 @@ export default () => {
             </div>
             <div className="card-content">
               <div className="row">
-                <div className="input-field col s4">
-                  <input placeholder="Your email" id="email" type="text" className="validate" />
-                  <label for="disabled">Email</label>
+                <div className="input-field col">
+                  <input placeholder="Your email" id="email" type="text" className="validate" name="email" onChange={handleChange} value={form.email} />
+                  <label htmlFor="disabled">Email</label>
                 </div>
               </div>
               <div className="row">
-                <div className="input-field col s4">
-                  <input placeholder="Your password" id="password" type="password" className="validate" />
-                  <label for="password">Password</label>
+                <div className="input-field col">
+                  <input placeholder="Your password" id="password" type="password" className="validate" name="password" onChange={handleChange} value={form.password} />
+                  <label htmlFor="password">Password</label>
                 </div>
               </div>
             </div>
             <div className="card-action">
-                  <a className="waves-effect waves-light btn mr-6">Sign in</a>
-                  <a className="waves-effect waves-light btn">Register</a>
+              <a className="waves-effect waves-light btn mr-6" disabled={isLoading}>Sign in</a>
+              <a className="waves-effect waves-light btn" onClick={handleRegister} disabled={isLoading}>Register</a>
             </div>
           </div>
         </div>
       </div>
-      
     </div>
   )
 };
