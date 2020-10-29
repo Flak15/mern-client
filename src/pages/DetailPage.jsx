288ae054d31e2eb90025c76ad00b2464568e1040
flak15 <flak160@gmail.com>
2020-10-29 14:02:37 +0300

    detail page

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export default () => {
+    const [link, setLink] = useState(null);
+    const { id } = useParams();
     return (
         <div>
             <h1>Detail Page</h1>
+            <div>Link id: {id}</div>
         </div>
     )
 };
